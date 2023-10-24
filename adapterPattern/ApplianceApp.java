63aa9960f5d9728409ebf6956a7dd085ee6efa72
Britney <142373706+BritneyBeligan@users.noreply.github.com>
2023-10-24 12:22:43 +0800

    Update ApplianceApp.java

--- adapterPattern/ApplianceApp.java
+++ adapterPattern/ApplianceApp.java
@@ -1,8 +1,19 @@
 package adapterPattern;
 
 public class ApplianceApp {
-
     public static void main(String[] args){
+
+        //laptop
+        Laptop laptop = new Laptop();
+        PowerOutlet lapTopRc = new LaptopAdapter(laptop);
+
+        //ref
+        Refrigerator refrigerator = new Refrigerator();
+        PowerOutlet refrigeratorRc = new RefrigeratorAdapter(refrigerator);
+
+        //smartphone
+        SmartphoneCharger smartphoneCharger = new SmartphoneCharger();
+        PowerOutlet smartphoneRc = new SmartphoneAdapter(smartphoneCharger);
 
         Tv tv = new Tv();
         RemoteControllerFunction tvRc = new TvAdapter(tv);
@@ -13,6 +24,13 @@ public class ApplianceApp {
         Speaker speaker = new Speaker();
         RemoteControllerFunction speakerRc = new SpeakerAdapter(speaker);
 
+        System.out.println();
+        //plugging in the appliances
+        System.out.println(lapTopRc.plugIn()); //laptop
+        System.out.println(refrigeratorRc.plugIn()); //refrigerator
+        System.out.println(smartphoneRc.plugIn()); //smartPhone
+
+        System.out.println();
         //turn on the appliances
         System.out.println(tvRc.powerOn());
         System.out.println(airConRc.powerOn());
